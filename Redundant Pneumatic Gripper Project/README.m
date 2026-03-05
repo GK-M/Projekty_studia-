beaf77de7f4203b0abd576b5545559018cb64eb7
G__K <93984678+GK-M@users.noreply.github.com>
2026-03-05 14:09:49 +0100

    Create README.m

---
+++ Redundant Pneumatic Gripper Project/README.m
@@ -0,0 +1,8 @@
+# Redundant Pneumatic Gripper Project
+## Project scope
+- Mechanical concept and design of the gripper
+- Pneumatic circuit design (including redundancy concept)
+- Selection of key components (actuators, valves, fittings, sensors)
+- Safety and reliability considerations
+- Documentation 
+
